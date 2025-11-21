f80dda2d4c28b50f104c96a8bd4d2a50b931bcbb
Michal Blava <michal.blava@gmail.com>
2025-11-21 08:18:02 +0100

    updated footer

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -8,13 +8,18 @@ defineProps<Props>();
 <template>
     <main class="max-w flex justify-center opacity-100 transition-opacity duration-750 starting:opacity-0">
         <div class="flex flex-col justify-center text-center dark:text-white z-10 gap-2 my-6 w-full max-w-[335px] text-sm lg:max-w-4xl">
-            <div>
-                Rezervačný systém pre <b>Farský ples Čierna Voda</b> vyvinul
-                <a href="https://hostnow.cz/" target="_blank" class="underline">Michal Barnáš</a>
-                &copy; {{ new Date().getFullYear() }}
+            <div class="mb-4">
+                V prípade technických problémov kontaktuj <a href="mailto:podpora@farskyplesciernavoda.sk" target="_blank" class="underline">podpora@farskyplesciernavoda.sk</a>
             </div>
             <div>
-                Našiel si chybu? Nahlás ju na <a href="https://github.com/mrgopes/farsky-ples-event-reservation" target="_blank" class="underline">GitHube</a> alebo vytvor PR :)
+                <div>
+                    Rezervačný systém pre <b>Farský ples Čierna Voda</b> vyvinul
+                    <a href="mailto:michal.barnas@hostnow.cz" target="_blank" class="underline">Michal Barnáš</a>
+                    &copy; {{ new Date().getFullYear() }}
+                </div>
+                <div>
+                    Zdrojový kód nájdeš na <a href="https://github.com/mrgopes/farsky-ples-event-reservation" target="_blank" class="underline"><i class="fab fa-github"></i> GitHube</a>.
+                </div>
             </div>
         </div>
     </main>
